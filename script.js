3cafc5e5d4cdd1702ccaddde83a5594eabf732a2
bh109 <119568902+bh109@users.noreply.github.com>
2025-11-19 22:57:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function loadIP() {
     const output = document.getElementById("output");
     output.innerText = "Loading...";
 
-    fetch("https://api.ipify.org?format=json")
+    fetch("https://api.ip.sb/jsonip")
         .then(res => res.json())
         .then(data => {
             output.innerText = "Your IPv4: " + data.ip;
         })
         .catch(err => {
-            output.innerText = "Failed to load IP: " + err;
+            output.innerText = "Failed to load IP:\n" + err;
         });
 }
